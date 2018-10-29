682d41d96c3dd22bf5b228dbbc2520f1e0e09734
HyungJu Na <nhj7@HyungJuui-MacBook-Pro.local>
2018-10-29 21:35:10 +0900

    update. self. vote.

--- app.js
+++ app.js
@@ -773,10 +773,12 @@ function wrkBot(){
             let self_weight = weight * 0.4;
             steem.broadcast.vote(wif, botList[i].id, parentAuthor, parentPermlink, weight - self_weight ,function(err, result) {
               logger.info('vote1!', err, result);
+              self_vote = function(){
+                steem.broadcast.vote(wif, botList[i].id, author, permlink, self_weight, function(err1, result1) { logger.info('vote2!', err1, result1); });
+              }
               setTimeout(
-                  function(){steem.broadcast.vote(wif, botList[i].id, author, permlink, self_weight, function(err1, result1) { logger.info('vote2!', err1, result1); })}
+                  self_vote();
               , 3500);
-
             });
 
           }
