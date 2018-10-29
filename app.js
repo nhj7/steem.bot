29d3d0ddf8f333c9018d516e85f77d24438f5d46
HyungJu Na <nhj7@HyungJuui-MacBook-Pro.local>
2018-10-29 21:40:07 +0900

    sel vote. add try~

--- app.js
+++ app.js
@@ -774,7 +774,12 @@ function wrkBot(){
             steem.broadcast.vote(wif, botList[i].id, parentAuthor, parentPermlink, weight - self_weight ,function(err, result) {
               logger.info('vote1!', err, result);
               let self_vote = function(){
-                steem.broadcast.vote(wif, botList[i].id, author, permlink, self_weight, function(err1, result1) { logger.info('vote2!', err1, result1); });
+                logger.info("vote2 start...");
+                try{
+                    steem.broadcast.vote(wif, botList[i].id, author, permlink, self_weight, function(err1, result1) { logger.info('vote2!', err1, result1); });
+                }catch(e){
+                  logger.error(e);
+                }
               }
               setTimeout(
                   self_vote
