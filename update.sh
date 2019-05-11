52b275c8b2f5fad191f1f361db1eff4f20fb814b
nhj7-home-win <nhj7@naver.com>
2019-05-11 21:36:45 +0900

    update.sh 업데이트.

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ git stash
 git pull
 forever stop app.js
 forever start app.js
-forever stop ./server/websocket.js
-forever start ./server/websocket.js
+#forever stop ./server/websocket.js
+#forever start ./server/websocket.js
 echo '★★★★★★★★★★★★★★★★★★★★ Node & Steem update complete. ★★★★★★★★★★★★★★★★★★★★'
