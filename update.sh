3772d7e715a21423902d54f16ed55cbf0bbcfe58
nhj <nhj7@naver.com>
2019-05-16 14:39:07 +0900

    commit update.sh

--- update.sh
+++ update.sh
@@ -7,3 +7,7 @@ forever start app.js
 #forever stop ./server/websocket.js
 #forever start ./server/websocket.js
 echo '★★★★★★★★★★★★★★★★★★★★ Node & Steem update complete. ★★★★★★★★★★★★★★★★★★★★'
+
+#add alias
+#vi ~/.bashrc
+#alias log='tail -f /log/steem_nhj/console.log.`date '+%Y.%m.%d'`'
